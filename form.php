7368508e72674bb913d2e3d5ce315f9239622acf
Ash_coder <69451974+karthi-Ash@users.noreply.github.com>
2021-04-15 12:49:12 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$mobile = $_POST['mobile'];
+$message = $_POST['message'];
+$to = "karthivijay0987@gmail.com";
+$txt = "Name = " . $name . "\r\nEmail = " . $email . "\r\nMobile =" . $mobile . "\r\nMessage =" . $message;
+$headers = "From: " . $email;
+if ($email != NULL) {
+    mail($to, $subject, $txt, $headers);
+    echo '<script>alert("Message Send Successfully")</script>';
+} else {
+    echo '<script>alert("Something Went Wrong")</script>';
+}
+header('location:portfolio.html');
